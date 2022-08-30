f4c596ba7bbdc0deb0e65add45db8a70ce456555
TheSwagg <648552952@qq.com>
2022-08-30 11:56:01 +0800

    TCR_1

---
+++ build/CMakeFiles/RM.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tkk-robort/RM_2/AngleSolver/AngleSolver_2.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/AngleSolver/AngleSolver_2.cpp.o"
+  "/home/tkk-robort/RM_2/AngleSolver/Filter.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/AngleSolver/Filter.cpp.o"
+  "/home/tkk-robort/RM_2/Armor_recognition/ArmorBox.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/Armor_recognition/ArmorBox.cpp.o"
+  "/home/tkk-robort/RM_2/Armor_recognition/ArmorDetector.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/Armor_recognition/ArmorDetector.cpp.o"
+  "/home/tkk-robort/RM_2/Armor_recognition/ArmorNumClassifier.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/Armor_recognition/ArmorNumClassifier.cpp.o"
+  "/home/tkk-robort/RM_2/Armor_recognition/LightBar.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/Armor_recognition/LightBar.cpp.o"
+  "/home/tkk-robort/RM_2/Armor_recognition/findLights.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/Armor_recognition/findLights.cpp.o"
+  "/home/tkk-robort/RM_2/Armor_recognition/matchArmors.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/Armor_recognition/matchArmors.cpp.o"
+  "/home/tkk-robort/RM_2/DaHengCamera/DaHengCamera.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/DaHengCamera/DaHengCamera.cpp.o"
+  "/home/tkk-robort/RM_2/Thread/ImageProcess.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/Thread/ImageProcess.cpp.o"
+  "/home/tkk-robort/RM_2/Thread/RemoteController.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/Thread/RemoteController.cpp.o"
+  "/home/tkk-robort/RM_2/buff_src/BuffAngleSolver.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/buff_src/BuffAngleSolver.cpp.o"
+  "/home/tkk-robort/RM_2/buff_src/FindBuff.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/buff_src/FindBuff.cpp.o"
+  "/home/tkk-robort/RM_2/global/General.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/global/General.cpp.o"
+  "/home/tkk-robort/RM_2/global/serialport.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/global/serialport.cpp.o"
+  "/home/tkk-robort/RM_2/main.cpp" "/home/tkk-robort/RM_2/build/CMakeFiles/RM.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  ".././header"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ run.sh
@@ -0,0 +1,2 @@
+cd /home/tkk-robort/RM_2/build
+./RM
